9363ea12e74110c03446ee6e4722042c7b360431
Andrea Rossi <a.rossi189@campus.unimib.it>
2024-10-09 05:55:05 +0200

    Started working on paper

--- Exercises/Module 1: Numbers/error_propagation_es/error_propagation.c
+++ Exercises/Module 1: Numbers/error_propagation_es/error_propagation.c
@@ -3,14 +3,14 @@
 int main() {
 
     // NOTE: la somma e' commutativa per valori in questo range di decimali
-    double x = (0.7 + 0.1) + 0.3;
-    double y = 0.7 + (0.1 + 0.3);
+    float x = (0.7 + 0.1) + 0.3;
+    float y = 0.7 + (0.1 + 0.3);
 
     printf("Left: %.16f, Right: %.16f \n", x, y);
 
-    double xt = 1.e20;
-    double yt = -1.e20;
-    double zt = 1;
+    float xt = 1.e20;
+    float yt = -1.e20;
+    float zt = 1;
 
     // NOTE: la somma non e' piu' commutativa per valori cosi' alti e' necessaria
     // una maggiore precisione (per esempio una quadrupla precisione) per far 
---
+++ Exercises/Module 1: Numbers/number_precision_es/number_precision.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int main() {
+
+#ifdef EXERCISE_FORMAT
+  // Exact exercise request
+
+  float f = 1.2e34;
+  printf("Single precision multiplication: \n");
+
+  for (int i = 0; i < 24; i++) {
+    f *= 2;
+    printf("%.20e\n", f);
+  }
+
+  double d = 1.2e304;
+  printf("Double precision multiplication: \n");
+
+  for (int i = 0; i < 24; i++) {
+    d *= 2;
+    printf("%.20e\n", d);
+  }
+
+  d = 1e-13;
+  printf("Double precision division: \n");
+
+  for (int i = 0; i < 24; i++) {
+    d /= 2;
+    printf("%.20e, %.20e \n", d, (double)(1 + d));
+  }
+
+  f = 1e-13;
+  printf("Single precision division: \n");
+
+  for (int i = 0; i < 24; i++) {
+    f /= 2;
+    printf("%.20e, %.20e \n", f, (float)(1 + f));
+  }
+#else
+  // Rewriting for gnuplot parsing
+
+  float f_mult = 1.2e34;
+  double d_mult = 1.2e304;
+  float f_div = 1e-13;
+  double d_div = 1e-13;
+
+  printf("f_mult\td_mult\td_div\t1+d_div\tf_div\t1+f_div\n");
+
+  for (int i = 0; i < 24; i++) {
+    f_mult *= 2;
+    d_mult *= 2;
+    f_div /= 2;
+    d_div /= 2;
+
+    printf("%.20e\t%.20e\t%.20e\t%.20e\t%.20e\t%.20e\n", f_mult, d_mult, d_div,
+           1 + d_div, f_div, 1 + f_div);
+  }
+#endif
+}
